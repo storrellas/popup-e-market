8e2b1fab2c9cdcb732a33bf1f4d9c2270d8547d8
storrellas <storrellas@gmail.com>
2020-06-17 12:28:05 +0000

    ENH: Adding fadeIn effect

--- modal/configure.js
+++ modal/configure.js
@@ -1,5 +1,62 @@
 
+
+
 class Modal {
+
+
+  // Show an element
+  static fadeIn (elem) {
+
+    // Get the natural height of the element
+    var getHeight = function () {
+      elem.style.display = 'block'; // Make it visible
+      var height = elem.scrollHeight + 'px'; // Get it's height
+      elem.style.display = ''; //  Hide it again
+      return height;
+    };
+
+    var height = getHeight(); // Get the natural height
+    elem.classList.add('is-visible'); // Make the element visible
+    elem.style.height = height; // Update the max-height
+
+    // Once the transition is complete, remove the inline max-height so the content can scale responsively
+    window.setTimeout(function () {
+      elem.style.height = '';
+    }, 350);
+
+  };
+
+  // Hide an element
+  static fadeOut (elem) {
+
+    // Give the element a height to change from
+    elem.style.height = elem.scrollHeight + 'px';
+
+    // Set the height back to 0
+    window.setTimeout(function () {
+      elem.style.height = '0';
+    }, 1);
+
+    // When the transition is complete, hide it
+    window.setTimeout(function () {
+      elem.classList.remove('is-visible');
+    }, 350);
+
+  };
+
+  // Toggle element visibility
+  static fadeToggle (elem, timing) {
+
+    // If the element is visible, hide it
+    if (elem.classList.contains('is-visible')) {
+      Modal.fadeOut(elem);
+      return;
+    }
+
+    // Otherwise, show it
+    Modal.fadeIn(elem);
+    
+  };
 
   static configure(){
     var btn = document.getElementById("buy-button");
@@ -28,6 +85,7 @@ class Modal {
     // create a new div element 
     var contentDiv = document.createElement("div"); 
     contentDiv.id = "content"
+    contentDiv.classList.add('toggle-content');
     document.body.appendChild(contentDiv); 
   
     fetch('/modal/modal.html')
@@ -38,10 +96,26 @@ class Modal {
   
         // Get elements
         Modal.configure()
-  
+
+        // Fade In
+        window.setTimeout(function(){
+          // Get the content
+          var content = document.getElementById("content");
+
+          // Toggle the content
+          Modal.fadeToggle(content);
+
+        }, 1000)
+
       });
   }
 
 }
+
+
+
+
+
+
 
 
